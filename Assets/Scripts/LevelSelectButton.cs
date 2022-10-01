99e25111f74fcfe3c5ae6680a39d95ad580407b0
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-10-01 15:45:05 -0300

    Level Select Screen (works)

---
+++ Assets/Scripts/LevelSelectButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public GameObject star1, star2, star3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
