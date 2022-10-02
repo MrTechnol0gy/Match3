023df7114966e7d3c2ebae5b5c85f934fae3b499
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-10-02 12:02:07 -0300

    Showing Stars and browsing levels

--- Assets/Scripts/LevelSelectButton.cs
+++ Assets/Scripts/LevelSelectButton.cs
@@ -12,7 +12,22 @@ public class LevelSelectButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        star1.SetActive(false); //sets the star to be deactive when the scene loads
+        star2.SetActive(false);
+        star3.SetActive(false);
+
+        if(PlayerPrefs.HasKey(levelToLoad + "_Star1")) //checks to see if the key exists
+        {
+            star1.SetActive(true);
+        }
+        if(PlayerPrefs.HasKey(levelToLoad + "_Star2")) //checks to see if the key exists
+        {
+            star2.SetActive(true);
+        }
+        if(PlayerPrefs.HasKey(levelToLoad + "_Star3")) //checks to see if the key exists
+        {
+            star3.SetActive(true);
+        }
     }
 
     public void LoadLevel()
